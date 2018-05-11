71ff25972ce113e6a1cdc456e0e971529353c51e
Greavesy1899 <connor.greaves1999@gmail.com>
2018-05-11 19:12:19 +0100

    Added new features to NifScan

--- src/commands/nif/NifScan.cpp
+++ src/commands/nif/NifScan.cpp
@@ -11,7 +11,7 @@ void BeginScan()
 {
 	Log::Info("Begin Scan");
 
-	fs::path nif = nif_in / "farmhouse06.nif";
+	fs::path nif = nif_in / "LongSword.nif";
 
 	NifInfo info;
 	vector<NiObjectRef> blocks = ReadNifList(nif.string().c_str(), &info);
@@ -36,12 +36,47 @@ void BeginScan()
 			}
 		}
 
-		if (blocks[i]->IsSameType(NiTriShapeData::TYPE)) {
-			//scan vertex for useless #FFFFFFFF
+		if (blocks[i]->IsSameType(BSLightingShaderProperty::TYPE)) {
+			BSLightingShaderPropertyRef  shaderprop = DynamicCast<BSLightingShaderProperty>(blocks[i]);
+
+			if (shaderprop->GetSkyrimShaderType() == (BSLightingShaderPropertyShaderType::ST_ENVIRONMENT_MAP) || (BSLightingShaderPropertyShaderType::ST_EYE_ENVMAP)) {
+				if ((shaderprop->GetShaderFlags1_sk() & SkyrimShaderPropertyFlags1::SLSF1_ENVIRONMENT_MAPPING) != SkyrimShaderPropertyFlags1::SLSF1_ENVIRONMENT_MAPPING) {
+					Log::Info("Block[%i]: ShaderType is 'Environment', but ShaderFlags1 does not include 'Environment Mapping'.", i);
+				}
+				if ((shaderprop->GetShaderFlags2_sk() & SkyrimShaderPropertyFlags2::SLSF2_GLOW_MAP) == SkyrimShaderPropertyFlags2::SLSF2_GLOW_MAP) {
+					Log::Info("Block[%i]: ShaderType is 'Environment', but ShaderFlags 2 has enabled 'glow' flag.", i);
+				}
+				if (shaderprop->GetTextureSet()->GetTextures().size() >= 5) {
+					if (shaderprop->GetTextureSet()->GetTextures()[4] != "") {
+						Log::Info("Block[%i]: ShaderType is 'Environment', but no 'Environment' texture is present.", i);
+					}
+				}
+				else {
+					Log::Info("Block[%i]: TextureSet size is too small to include 'Environment' texture.", i);
+				}
+			}
+		}
+
+		if (blocks[i]->IsSameType(NiTriShape::TYPE)) {
+			NiTriShapeRef shape = DynamicCast<NiTriShape>(blocks[i]);
+			NiTriShapeDataRef data = DynamicCast<NiTriShapeData>(shape->GetData());
+
+			if ((data->GetHasVertexColors()) && (shape->GetShaderProperty()->GetShaderFlags2() & SkyrimShaderPropertyFlags2::SLSF2_TREE_ANIM) != SkyrimShaderPropertyFlags2::SLSF2_TREE_ANIM) {
+				vector<Color4> vc = data->GetVertexColors();
+				bool allWhite = true;
+				for (int i = 0; i != vc.size(); i++) {
+					if (vc[i].r != 1.0f || vc[i].g != 1.0f || vc[i].b != 1.0f) {
+						allWhite = false;
+					}
+				}
+				if (allWhite)
+					Log::Info("Block[%i]: Redundant all white #FFFFFFFF vertex colors.", i);
+			}
+
+
 			//scan if normals are incorrect
 			//scan if NiAlphaProperty is below BSLightingShaderProperty
 			//scan to check if vertex is checked AND shader flag is.
-			//scan environment mapping shader AND shader flag.
 		}
 
 		//check if mesh uses SLSF1_External_Emittance and then if bit 9 is set.
