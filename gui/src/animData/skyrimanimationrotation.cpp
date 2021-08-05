507e764275803042777df088101b8d5ab44b71f1
e.canepa <e.canepa@isacstl.it>
2021-08-05 08:11:28 +0100

    Purge the unuseful Zartar's GUI. R.I.P

--- gui/src/animData/skyrimanimationrotation.cpp
+++
@@ -1,7 +0,0 @@
-#include "skyrimanimationrotation.h"
-
-SkyrimAnimationRotation::SkyrimAnimationRotation(qreal time, qreal c2, qreal c3, qreal c4, qreal c5)
-    : localTime(time), x(c2), y(c3), z(c4), w(c5)
-{
-    //
-}
